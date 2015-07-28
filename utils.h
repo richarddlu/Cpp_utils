aef686e03d7711dd59b3fdb58e276ac1fa3217af
Lu Sun <sunlufinal@hotmail.com>
2015-07-28 13:14:43 -0400

    move definition of rSampling to header
    version 0.01

--- utils.h
+++ utils.h
@@ -3,19 +3,23 @@
  *
  * @author: Lu Sun
  * 
- * Version: 0
+ * Version: 0.01
  */
 
 #ifndef __CPP_UTILS__
 #define __CPP_UTILS__
 
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 /**
  * reservoir sampling
  * samples S distinct elements from src randomly
+ *
+ * The definition is put in the header file because of a C++ template problem
  *
  * refreence: http://stackoverflow.com/questions/54059/efficiently-selecting-a-set-of-random-elements-from-a-linked-list
  *
@@ -24,6 +28,29 @@ using namespace std;
  * @param size_t S number of selected elements
  */
 template <typename T>
-void rSampling(const vector<T>& src, vector<T>& dst, size_t S);
+void rSampling(const vector<T>& src, vector<T>& dst, size_t S) {
+	// parameter validation
+	if(!(S > 0))
+		return;
+	if(src.size() < S)
+		return;
+
+	// clear dst
+	dst.clear();
+
+	for(size_t i = 0; i < S; i++) {
+		dst.push_back(src[i]);
+	}
+
+	size_t n = src.size();	// performance consideration, avoid calling function
+	size_t num_seen = S;
+	srand(time(NULL));
+	while(num_seen < n) {
+		num_seen++;
+		int rn = rand() % num_seen;
+		if(rn < S)
+			dst[rn] = src[num_seen-1];
+	}
+}
 
 #endif
