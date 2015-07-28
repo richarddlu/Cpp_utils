84df4c36a32b11a3ec18b2bdaddeedf697f53935
Lu Sun <sunlufinal@hotmail.com>
2015-07-28 13:41:12 -0400

    rSamplingInt
    version 0.02
    
    Implemented rSamplingInt
    
    Use rSamplingInt in rSampling

--- utils.cpp
+++ utils.cpp
@@ -3,7 +3,38 @@
  *
  * @author: Lu Sun
  * 
- * Version: 0
+ * Version: 0.02
  */
 
 #include "utils.h"
+
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void rSamplingInt(vector<int>& samples, int a, int b, size_t S) {
+	// parameter validation
+	if(!(S > 0))
+		return;
+	if(b - a < S)
+		return;
+
+	// clear dst
+	samples.clear();
+
+	for(size_t i = 0; i < S; i++) {
+		samples.push_back(a + i);
+	}
+
+	size_t n = b - a;
+	size_t num_seen = S;
+	srand(time(NULL));
+	while(num_seen < n) {
+		num_seen++;
+		int rn = rand() % num_seen;
+		if(rn < S)
+			samples[rn] = a + num_seen - 1;
+	}
+}
--- utils.h
+++ utils.h
@@ -3,17 +3,28 @@
  *
  * @author: Lu Sun
  * 
- * Version: 0.01
+ * Version: 0.02
  */
 
 #ifndef __CPP_UTILS__
 #define __CPP_UTILS__
 
 #include <vector>
-#include <cstdlib>
-#include <ctime>
 
 using namespace std;
+
+/**
+ * reservoir sampling
+ * generate S distinct integers in [a, b)
+ *
+ * refreence: http://stackoverflow.com/questions/54059/efficiently-selecting-a-set-of-random-elements-from-a-linked-list
+ *
+ * @param const vector<int>& samples output vector
+ * @param int a
+ * @param int b
+ * @param size_t S number of samples
+ */
+void rSamplingInt(vector<int>& samples, int a, int b, size_t S);
 
 /**
  * reservoir sampling
@@ -35,22 +46,14 @@ void rSampling(const vector<T>& src, vector<T>& dst, size_t S) {
 	if(src.size() < S)
 		return;
 
+	vector<int> indices;
+	rSamplingInt(indices, 0, src.size(), S);
+
 	// clear dst
 	dst.clear();
 
-	for(size_t i = 0; i < S; i++) {
-		dst.push_back(src[i]);
-	}
-
-	size_t n = src.size();	// performance consideration, avoid calling function
-	size_t num_seen = S;
-	srand(time(NULL));
-	while(num_seen < n) {
-		num_seen++;
-		int rn = rand() % num_seen;
-		if(rn < S)
-			dst[rn] = src[num_seen-1];
-	}
+	for(size_t i = 0; i < S; i++)
+		dst.push_back(src[indices[i]]);
 }
 
 #endif
